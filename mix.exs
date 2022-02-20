fdcde3769450c8189c968991d57b8d4f4f871d47
Christopher Grainger <chris@amplified.ai>
2022-02-21 10:07:23 +1100

    set required elixir to 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Scholar.MixProject do
     [
       app: :scholar,
       version: "0.1.0",
-      elixir: "~> 1.14-dev",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs()
