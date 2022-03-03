01e9045ea2caffd27ed6cf14fc3bd8131a20e365
Tiago Davi <tiago.asp.net@gmail.com>
2022-03-03 18:59:51 -0300

    add standard scaler (#2)

---
+++ test/scholar/preprocessing_test.exs
@@ -0,0 +1,26 @@
+defmodule Scholar.PreprocessingTest do
+  use ExUnit.Case, async: true
+
+  alias Scholar.Preprocessing
+
+  describe "standard_scaler/1" do
+    test "applies standard scaling to data" do
+      data = Nx.tensor([[1, -1, 2], [2, 0, 0], [0, 1, -1]])
+
+      expected =
+        Nx.tensor([
+          [0.5212860703468323, -1.3553436994552612, 1.4596009254455566],
+          [1.4596009254455566, -0.4170288145542145, -0.4170288145542145],
+          [-0.4170288145542145, 0.5212860703468323, -1.3553436994552612]
+        ])
+
+      assert expected == Preprocessing.standard_scaler(data)
+    end
+
+    test "leaves data as it is when variance is zero" do
+      data = 42.0
+      expected = Nx.tensor(data)
+      assert expected == Preprocessing.standard_scaler(data)
+    end
+  end
+end
