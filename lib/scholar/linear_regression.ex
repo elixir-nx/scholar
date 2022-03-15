ff24acad226b1f4d8fac29da20002fed12ee7fcf
Sean Moriarity <smoriarity.5@gmail.com>
2022-03-15 19:19:24 -0400

    Start linreg

---
+++ lib/scholar/linear_regression.ex
@@ -0,0 +1,40 @@
+defmodule Scholar.LinearRegression do
+
+  @doc """
+  Fits a linear regression model to the given dataset.
+  """
+  def fit(x, y) do
+    {coeff, intercept} = lstsq(x, y)
+  end
+
+  defnp lstsq(x, y) do
+    {u, s, vt} = Nx.LinAlg.svd(x)
+
+    uty =
+      u
+      |> Nx.transpose()
+      |> Nx.dot(y)
+
+    s_inv = Nx.new_axis(1 / s, -1)
+
+    x =
+      vt
+      |> Nx.transpose()
+      |> Nx.dot(Nx.multiply(s_inv, uty))
+
+    Nx.transpose(x)
+  end
+
+  defimpl Scholar do
+    import Nx.Defn
+
+    def predict(%LinearRegression{coefficients: coef}, x) do
+      decision_function(coeff, intercept, x)
+    end
+
+    defnp decision_function(coeff, x) do
+      x
+      |> Nx.dot(coeff)
+    end
+  end
+end
