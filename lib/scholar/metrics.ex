9ab6ad2796f6bf79913c4f2d33ed189cbab0ad64
Cocoa <r.xu.1@research.gla.ac.uk>
2022-05-04 19:26:06 +0100

    added some multi-class examples

--- lib/scholar/metrics.ex
+++ lib/scholar/metrics.ex
@@ -78,6 +78,39 @@ defmodule Scholar.Metrics do
   end
 
   @doc ~S"""
+  Computes the accuracy of the given predictions
+  for multi-class classification problems.
+
+  ## Argument Shapes
+
+    * `y_true` - $\(d_0, d_1, ..., d_n\)$
+    * `y_pred` - $\(d_0, d_1, ..., d_n\)$
+
+  ## Examples
+
+      iex> y_true = Nx.tensor([0, 1, 1, 1, 1, 0, 2, 1, 0, 1], type: {:u, 32})
+      iex> y_pred = Nx.tensor([0, 2, 1, 1, 2, 2, 2, 0, 0, 1], type: {:u, 32})
+      iex> Scholar.Metrics.accuracy(y_true, y_pred, num_classes: 3)
+      #Nx.Tensor<
+        f32[3]
+        [0.6666666865348816, 1.0, 0.25]
+      >
+
+  """
+  defn accuracy(y_true, y_pred, opts \\ []) do
+    opts = keyword!(opts, [:num_classes])
+    assert_shape_pattern(y_true, {_})
+    assert_shape(y_pred, Nx.shape(y_true))
+
+    cm = confusion_matrix(y_true, y_pred, opts)
+
+    true_positive = Nx.take_diagonal(cm)
+    false_positive = Nx.subtract(Nx.sum(cm, axes: [0]), true_positive)
+
+    Nx.divide(true_positive, true_positive + false_positive + 1.0e-16)
+  end
+
+  @doc ~S"""
   Computes the precision of the given predictions with respect to
   the given targets for binary classification problems.
 
@@ -143,7 +176,7 @@ defmodule Scholar.Metrics do
     cm = confusion_matrix(y_true, y_pred, opts)
     true_positive = Nx.take_diagonal(cm)
     false_positive = Nx.subtract(Nx.sum(cm, axes: [0]), true_positive)
-    Nx.divide(true_positive, Nx.add(true_positive, false_positive))
+    Nx.divide(true_positive, true_positive + false_positive + 1.0e-16)
   end
 
   @doc ~S"""
@@ -211,7 +244,7 @@ defmodule Scholar.Metrics do
     cm = confusion_matrix(y_true, y_pred, opts)
     true_positive = Nx.take_diagonal(cm)
     false_negative = Nx.subtract(Nx.sum(cm, axes: [1]), true_positive)
-    Nx.divide(true_positive, Nx.add(true_positive, false_negative))
+    Nx.divide(true_positive, true_positive + false_negative + 1.0e-16)
   end
 
   @doc """
@@ -398,7 +431,7 @@ defmodule Scholar.Metrics do
     true_positive = Nx.take_diagonal(cm)
     false_positive = Nx.subtract(Nx.sum(cm, axes: [0]), true_positive)
     false_negative = Nx.subtract(Nx.sum(cm, axes: [1]), true_positive)
-    Nx.subtract(Nx.sum(cm), Nx.add(Nx.add(false_negative, false_positive), true_positive))
+    Nx.subtract(Nx.sum(cm), false_negative + false_positive + true_positive)
   end
 
   @doc """
@@ -683,13 +716,29 @@ defmodule Scholar.Metrics do
 
       iex> cur_avg = 0.5
       iex> iteration = 1
-      iex> y_true = Nx.tensor([[0, 1], [1, 0], [1, 0]])
-      iex> y_pred = Nx.tensor([[0, 1], [1, 0], [1, 0]])
+      iex> y_true = Nx.tensor([0, 1, 1])
+      iex> y_pred = Nx.tensor([0, 1, 1])
       iex> avg_acc = Scholar.Metrics.running_average(&Scholar.Metrics.binary_accuracy/2)
       iex> avg_acc.(cur_avg, [y_true, y_pred], iteration)
       #Nx.Tensor<
         f32
         0.75
+      >
+      iex> y_true = Nx.tensor([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+      iex> y_pred = Nx.tensor([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+      iex> avg_acc = Scholar.Metrics.running_average(&Scholar.Metrics.onehot_accuracy/2)
+      iex> avg_acc.(cur_avg, [y_true, y_pred], iteration)
+      #Nx.Tensor<
+        f32
+        0.75
+      >
+      iex> y_true = Nx.tensor([0, 1, 2])
+      iex> y_pred = Nx.tensor([0, 1, 2])
+      iex> avg_acc = Scholar.Metrics.running_average(&Scholar.Metrics.accuracy(&1, &2, num_classes: 3))
+      iex> avg_acc.(cur_avg, [y_true, y_pred], iteration)
+      #Nx.Tensor<
+        f32[3]
+        [0.75, 0.75, 0.75]
       >
   """
   def running_average(metric) do
@@ -718,6 +767,16 @@ defmodule Scholar.Metrics do
       #Nx.Tensor<
         s64
         13
+      >
+      iex> cur_sum = Nx.tensor([10, 20, 30])
+      iex> iteration = 2
+      iex> y_true = Nx.tensor([0, 1, 2, 1])
+      iex> y_pred = Nx.tensor([1, 1, 2, 0])
+      iex> fps = Scholar.Metrics.running_sum(&Scholar.Metrics.false_positives(&1, &2, num_classes: 3))
+      iex> fps.(cur_sum, [y_true, y_pred], iteration)
+      #Nx.Tensor<
+        s64[3]
+        [11, 21, 30]
       >
   """
   def running_sum(metric) do
