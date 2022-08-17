5d24a61588db032dd6a1a700e8c1caadfd228387
José Valim <jose.valim@dashbit.co>
2022-08-17 14:22:10 +0200

    Update Nx version

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Scholar.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:nx, github: "elixir-nx/nx", sparse: "nx"},
+      {:nx, "~> 0.3"},
       {:explorer, "~> 0.2.0", only: [:test, :dev]}
     ]
   end
