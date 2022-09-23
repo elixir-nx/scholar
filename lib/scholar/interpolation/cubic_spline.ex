362e6ac70da4519f5c02a4d93cd74c48a89b35f2
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-09-23 10:57:27 -0300

    wip

--- lib/scholar/interpolation/cubic_spline.ex
+++ lib/scholar/interpolation/cubic_spline.ex
@@ -4,8 +4,8 @@ defmodule Scholar.Interpolation.CubicSpline do
   """
   import Nx.Defn
 
-  @derive {Nx.Container, containers: [:coefficients, :x, :y]}
-  defstruct [:coefficients, :x, :y]
+  @derive {Nx.Container, containers: [:coefficients, :x, :y, :h]}
+  defstruct [:coefficients, :x, :y, :h]
 
   opts = [
     extrapolate: [
@@ -25,10 +25,11 @@ defmodule Scholar.Interpolation.CubicSpline do
   #{NimbleOptions.docs(@opts_schema)}
   """
   deftransform train(x, y, opts \\ []) do
-    train_n(x, y, NimbleOptions.validate!(opts, @opts_schema))
+    train_n(x, y)
+    # train_n(x, y, NimbleOptions.validate!(opts, @opts_schema))
   end
 
-  defnp train_n(x, y, opts \\ []) do
+  defnp train_n(x, y) do
     # https://en.wikiversity.org/wiki/Cubic_Spline_Interpolation
 
     h_i = x[1..-1//1] - x[0..-2//1]
@@ -46,10 +47,14 @@ defmodule Scholar.Interpolation.CubicSpline do
       |> Nx.put_diagonal(μ_i, offset: -1)
       |> Nx.put_diagonal(lambda_i, offset: 1)
 
-    %__MODULE__{coefficients: Nx.LinAlg.solve(a, d_i), x: x}
+    coefs = Nx.LinAlg.solve(a, d_i)
+
+    zero = Nx.tensor([0])
+    coefs = Nx.concatenate([zero, coefs[1..-2//1], zero])
+    %__MODULE__{coefficients: coefs, x: x, y: y, h: h_i}
   end
 
-  defnp three_point_divided_difference(x, y) do
+  defnp three_point_divided_difference(x, y) do``
     x_minus = x[0..-3//1]
     x_i = x[1..-2//1]
     x_plus = x[2..-1//1]
@@ -68,11 +73,26 @@ defmodule Scholar.Interpolation.CubicSpline do
 
     f = (f_plus - f_minus) / (x_plus - x_minus)
 
-    zero = Nx.tensor([0])
-
-    Nx.concatenate([zero, f, zero])
+    # Force the initial conditions f''(0) = f''(n) = 0
+    # zero = Nx.tensor([0])
+    Nx.concatenate([Nx.new_axis(f[0], 0), f, Nx.new_axis(f[-1], 0)])
   end
 
-  defn predict(%__MODULE__{x: source_x, y: source_y, coefficients: coefficients}, x) do
+  defn predict(%__MODULE__{x: x, y: y, coefficients: coefficients, h: h}, target_x) do
+    i = Nx.sum(x < target_x)
+    i_prev = Nx.select(i > 1, i - 1, 0)
+    i = Nx.select(i > 1, i, 1)
+
+    # if i_prev < 0 == 1 do
+    #   raise "negative index #{inspect(i)}"
+    # end
+
+    m_prev = coefficients[i_prev]
+    m_i = coefficients[i]
+
+    c_i = m_prev * (x[i] - target_x) ** 3 / (6 * h[i])
+    c_i = c_i + m_i * (target_x - x[i_prev]) ** 3 / (6 * h[i])
+    c_i = c_i + (y[i_prev] - m_prev * h[i] ** 2 / 6) * (x[i] - target_x) / h[i]
+    c_i + (y[i] - m_i * h[i] ** 2 / 6) * (target_x - x[i_prev]) / h[i]
   end
 end
