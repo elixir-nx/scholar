4c3d12f2f6cdc3033062d39d67bbac9bc06f9ae9
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-09-23 16:17:10 -0300

    feat: add predict implementation

--- lib/scholar/interpolation/cubic_spline.ex
+++ lib/scholar/interpolation/cubic_spline.ex
@@ -24,7 +24,7 @@ defmodule Scholar.Interpolation.CubicSpline do
 
   #{NimbleOptions.docs(@opts_schema)}
   """
-  deftransform train(x, y, opts \\ []) do
+  deftransform train(x, y, _opts \\ []) do
     train_n(x, y)
     # train_n(x, y, NimbleOptions.validate!(opts, @opts_schema))
   end
@@ -33,15 +33,15 @@ defmodule Scholar.Interpolation.CubicSpline do
     # https://en.wikiversity.org/wiki/Cubic_Spline_Interpolation
     # Reference implementation in Scipy
 
-    # bc_type (boundary_condition_type) = 'not-a-knot'
-    # extrapolate = True
+    x = Nx.as_type(x, :f64)
+    y = Nx.as_type(y, :f64)
 
     dx = x[1..-1//1] - x[0..-2//1]
     n = Nx.size(x)
 
     slope = (y[1..-1//1] - y[0..-2//1]) / dx
 
-    coefs =
+    s =
       if n == 3 do
         a =
           Nx.concatenate(
@@ -101,34 +101,30 @@ defmodule Scholar.Interpolation.CubicSpline do
         Nx.LinAlg.solve(a, b)
       end
 
-    %__MODULE__{coefficients: coefs, x: x, y: y, dx: dx}
-  end
-
-  defn predict(%__MODULE__{x: x, y: y, coefficients: coefficients, dx: dx}, target_x) do
-    # i = Nx.sum(x < target_x)
-    # i_prev = Nx.select(i > 0, i - 1, 0)
-    # i = Nx.select(i > 0, i, 0)
-
-    # if i_prev < 0 == 1 do
-    #   raise "negative index #{inspect(i)}"
-    # end
-
-    # m_prev = coefficients[i_prev]
-    # m_i = coefficients[i]
-
-    # c_i = m_prev * (x[i] - target_x) ** 3 / (6 * h[i])
-    # c_i = c_i + m_i * (target_x - x[i_prev]) ** 3 / (6 * h[i])
-    # c_i = c_i + (y[i_prev] - m_prev * h[i] ** 2 / 6) * (x[i] - target_x) / h[i]
-    # c_i + (y[i] - m_i * h[i] ** 2 / 6) * (target_x - x[i_prev]) / h[i]
     slope = (y[1..-1//1] - y[0..-2//1]) / dx
-    t = (coefficients[0..-2//1] + coefficients[1..-1//1] - 2 * slope) / dx
-
+    t = (s[0..-2//1] + s[1..-1//1] - 2 * slope) / dx
 
     c_3 = t / dx
-    c_2 = (slope - coefficients[0..-2//1]) / dx - t
-    c_1 = coefficients[0..-2//1]
+    c_2 = (slope - s[0..-2//1]) / dx - t
+    c_1 = s[0..-2//1]
     c_0 = y[0..-2//1]
 
-    c_3[0] * target_x ** 3 + c_2[0] * target_x ** 2 + c_1[0] * target_x + c_0[0]
+    c = Nx.stack([c_3, c_2, c_1, c_0], axis: 0)
+
+    %__MODULE__{coefficients: c, x: x, y: y, dx: dx}
+  end
+
+  defn predict(%__MODULE__{x: x, coefficients: coefficients}, target_x) do
+    x = Nx.as_type(x, :f64)
+    target_x = Nx.as_type(target_x, :f64)
+    coefficients = Nx.as_type(coefficients, :f64)
+    idx = Nx.sum(x < target_x)
+    idx = Nx.min(idx, Nx.size(x) - 2)
+
+    c_poly = coefficients[[0..-1//1, idx]]
+
+    x_poly = target_x - x[idx]
+
+    Nx.dot(x_poly ** Nx.tensor([3.0, 2, 1, 0], type: :f64), c_poly)
   end
 end
