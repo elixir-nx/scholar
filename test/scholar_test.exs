c387b504955d7bebc740dfc472651609fac2c892
Mateusz <msluszniak@student.agh.edu.pl>
2022-12-22 01:09:55 +0100

    Add ExUnit.CaseTemplate

--- test/scholar_test.exs
+++ test/scholar_test.exs
@@ -1,2 +1,22 @@
 defmodule ScholarTest do
+  use ExUnit.CaseTemplate
+
+  def assert_all_close(left, right, opts \\ []) do
+    atol = opts[:atol] || 1.0e-7
+    rtol = opts[:rtol] || 1.0e-7
+
+    equals =
+      left
+      |> Nx.all_close(right, atol: atol, rtol: rtol)
+      |> Nx.backend_transfer(Nx.BinaryBackend)
+
+    if equals != Nx.tensor(1, type: {:u, 8}, backend: Nx.BinaryBackend) do
+      flunk("""
+      expected
+      #{inspect(left)}
+      to be within tolerance of
+      #{inspect(right)}
+      """)
+    end
+  end
 end
