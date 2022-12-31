ccd18fb6bb1966bfbe7f6296bd125408611335fc
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2022-12-31 04:34:13 +0100

    Add Linear Regression (#56)
    * Add Linear Regression
    
    * Apply suggestions from code review
    
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>
    
    * Update lib/scholar/linear/linear_regression.ex
    
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>
    
    * Change implementation of lstsq using pinv
    
    * Simplify code
    
    * Set Nx with pinv PR
    
    * Update package
    
    * Update usage of Nx.weighted_mean
    
    * Update testing
    
    * Simplify code
    
    * Add suggestions from code review
    
    Co-authored-by: Mateusz <msluszniak@student.agh.edu.pl>
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>

---
+++ lib/scholar/linear/linear_regression.ex
@@ -0,0 +1,89 @@
+defmodule Scholar.Linear.LinearRegression do
+  @moduledoc """
+  Ordinary least squares linear regression.
+  """
+  import Nx.Defn
+
+  @derive {Nx.Container, containers: [:coefficients]}
+  defstruct [:coefficients]
+
+  opts = [
+    sample_weights: [
+      type: {:list, {:custom, Scholar.Options, :positive_number, []}}
+    ]
+  ]
+
+  @opts_schema NimbleOptions.new!(opts)
+
+  @doc """
+  Fits a linear regression model for sample inputs `a` and
+  sample targets `b`.
+  """
+  deftransform fit(a, b, opts \\ []) do
+    opts = NimbleOptions.validate!(opts, @opts_schema)
+
+    opts =
+      [
+        sample_weights_flag: opts[:sample_weights] != nil
+      ] ++
+        opts
+
+    {sample_weights, opts} = Keyword.pop(opts, :sample_weights, 1.0)
+    sample_weights = Nx.tensor(sample_weights)
+
+    fit_n(a, b, sample_weights, opts)
+  end
+
+  defnp fit_n(a, b, sample_weights, opts \\ []) do
+    {a, b} = preprocess_data(a, b, sample_weights, opts)
+
+    {a, b} =
+      if opts[:sample_weights_flag] do
+        rescale(a, b, sample_weights)
+      else
+        {a, b}
+      end
+
+    coeff = lstsq(a, b)
+    %__MODULE__{coefficients: coeff}
+  end
+
+  @doc """
+  Makes predictions with the given model on inputs `x`.
+  """
+  defn predict(%__MODULE__{coefficients: coeff}, x) do
+    Nx.dot(x, coeff)
+  end
+
+  # Implements sample weighting by rescaling inputs and
+  # targets by sqrt(sample_weight).
+  defnp rescale(x, y, sample_weights) do
+    case Nx.shape(sample_weights) do
+      {} = scalar ->
+        scalar = Nx.sqrt(scalar)
+        {scalar * x, scalar * y}
+
+      _ ->
+        scale = sample_weights |> Nx.sqrt() |> Nx.make_diagonal()
+        {Nx.dot(scale, x), Nx.dot(scale, y)}
+    end
+  end
+
+  # Implements ordinary least-squares by estimating the
+  # solution A to the equation A.X = b.
+  defnp lstsq(a, b) do
+    pinv = Nx.LinAlg.pinv(a)
+    Nx.dot(b, [0], pinv, [1])
+  end
+
+  defnp preprocess_data(x, y, sample_weights, opts \\ []) do
+    {x_offset, y_offset} =
+      if opts[:sample_weights_flag],
+        do:
+          {Nx.weighted_mean(x, sample_weights, axes: [0]),
+           Nx.weighted_mean(y, sample_weights, axes: [0])},
+        else: {Nx.mean(x, axes: [0]), Nx.mean(y, axes: [0])}
+
+    {x - x_offset, y - y_offset}
+  end
+end
