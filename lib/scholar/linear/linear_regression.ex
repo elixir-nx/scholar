f0d7c40bc1835e2f6ecab6818ec1a2852df4a2b1
Sean Moriarity <smoriarity.5@gmail.com>
2023-02-28 18:24:11 -0800

    Update args (#84)

--- lib/scholar/linear/linear_regression.ex
+++ lib/scholar/linear/linear_regression.ex
@@ -61,7 +61,7 @@ defmodule Scholar.Linear.LinearRegression do
         5.896470069885254
       >
   """
-  deftransform fit(a, b, opts \\ []) do
+  deftransform fit(x, y, opts \\ []) do
     opts = NimbleOptions.validate!(opts, @opts_schema)
 
     opts =
@@ -73,7 +73,7 @@ defmodule Scholar.Linear.LinearRegression do
     {sample_weights, opts} = Keyword.pop(opts, :sample_weights, 1.0)
     sample_weights = Nx.tensor(sample_weights)
 
-    fit_n(a, b, sample_weights, opts)
+    fit_n(x, y, sample_weights, opts)
   end
 
   defnp fit_n(a, b, sample_weights, opts) do
