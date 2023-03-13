fde48bc0f984c62f320e4ca8f001ca6882b17e7e
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2023-03-13 22:09:43 +0100

    Fix logistic regression (#89)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,7 @@
 ExUnit.start()
 
+alias Explorer.DataFrame, as: DF
+
 defmodule Datasets do
   def get(key) do
     :persistent_term.get({__MODULE__, key})
@@ -8,36 +10,23 @@ defmodule Datasets do
   def put(key, x_train, x_test, y_train, y_test) do
     :persistent_term.put(
       {__MODULE__, key},
-      {df_to_matrix(x_train), df_to_matrix(x_test), df_to_vector(y_train), df_to_vector(y_test)}
+      {Nx.stack(x_train, axis: 1), Nx.stack(x_test, axis: 1), Nx.stack(y_train, axis: 1),
+       Nx.stack(y_test, axis: 1)}
     )
-  end
-
-  defp df_to_matrix(df) do
-    df
-    |> Explorer.DataFrame.names()
-    |> Enum.map(&(Explorer.Series.to_tensor(df[&1]) |> Nx.new_axis(-1)))
-    |> Nx.concatenate(axis: 1)
-  end
-
-  defp df_to_vector(df) do
-    case Explorer.DataFrame.names(df) do
-      [name] -> Explorer.Series.to_tensor(df[name])
-      _several -> df |> df_to_matrix() |> Nx.argmax(axis: 1)
-    end
   end
 end
 
 # Pima Indians Diabetes Data
-data = Explorer.DataFrame.from_csv!("test/data/pima.csv", header: false)
+data = DF.from_csv!("test/data/pima.csv", header: false)
 
-x = Explorer.DataFrame.discard(data, [-1])
-y = Explorer.DataFrame.select(data, [-1])
+x = DF.discard(data, [-1])
+y = DF.select(data, [-1])
 
-x_train = Explorer.DataFrame.slice(x, 0, 500)
-x_test = Explorer.DataFrame.slice(x, 500, 500)
+x_train = DF.slice(x, 0, 600)
+x_test = DF.slice(x, 600, 500)
 
-y_train = Explorer.DataFrame.slice(y, 0, 500)
-y_test = Explorer.DataFrame.slice(y, 500, 500)
+y_train = DF.slice(y, 0, 600)
+y_test = DF.slice(y, 600, 500)
 
 Datasets.put(:pima, x_train, x_test, y_train, y_test)
 
@@ -47,13 +36,13 @@ df = Explorer.Datasets.iris()
 train_ids = for n <- 0..149, rem(n, 5) != 0, do: n
 test_ids = for n <- 0..149, rem(n, 5) == 0, do: n
 
-x = Explorer.DataFrame.discard(df, ["species"])
-y = Explorer.DataFrame.select(df, ["species"]) |> Explorer.DataFrame.dummies(["species"])
+x = DF.discard(df, ["species"])
+y = DF.select(df, ["species"]) |> DF.dummies(["species"])
 
-x_train = Explorer.DataFrame.slice(x, train_ids)
-x_test = Explorer.DataFrame.slice(x, test_ids)
+x_train = DF.slice(x, train_ids)
+x_test = DF.slice(x, test_ids)
 
-y_train = Explorer.DataFrame.slice(y, train_ids)
-y_test = Explorer.DataFrame.slice(y, test_ids)
+y_train = DF.slice(y, train_ids)
+y_test = DF.slice(y, test_ids)
 
 Datasets.put(:iris, x_train, x_test, y_train, y_test)
