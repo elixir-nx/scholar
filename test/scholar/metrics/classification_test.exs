8ca6a320bf3edc5248e33af38d3c7e996a83ef5a
0urobor0s <0urobor0s@users.noreply.github.com>
2023-10-10 15:56:59 +0100

    F-beta score

--- test/scholar/metrics/classification_test.exs
+++ test/scholar/metrics/classification_test.exs
@@ -14,4 +14,24 @@ defmodule Scholar.Metrics.ClassificationTest do
     assert_all_close(tpr, Nx.tensor([0.0, 0.5, 1.0, 1.0]))
     assert_all_close(thresholds, Nx.tensor([1.3, 0.3, 0.2, 0.1]))
   end
+
+  describe "fbeta_score" do
+    test "equals recall when beta is infinity" do
+      beta = Nx.tensor(:infinity)
+      y_true = Nx.tensor([0, 0, 0, 0, 0, 1, 1, 1, 1, 1], type: :u32)
+      y_pred = Nx.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], type: :u32)
+      fbeta_scores = Classification.fbeta_score(y_true, y_pred, beta, num_classes: 2)
+
+      assert_all_close(fbeta_scores, Classification.recall(y_true, y_pred, num_classes: 2))
+    end
+
+    test "equals precision when beta is 0" do
+      beta = Nx.tensor(0)
+      y_true = Nx.tensor([0, 0, 0, 0, 0, 1, 1, 1, 1, 1], type: :u32)
+      y_pred = Nx.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], type: :u32)
+      fbeta_scores = Classification.fbeta_score(y_true, y_pred, beta, num_classes: 2)
+
+      assert_all_close(fbeta_scores, Classification.precision(y_true, y_pred, num_classes: 2))
+    end
+  end
 end
