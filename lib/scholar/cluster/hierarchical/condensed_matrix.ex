7021a93daf9514e5f0cf510cf485fd7916829e73
William Lanchantin <william.lanchantin@cargosense.com>
2023-10-14 13:13:49 -0400

    don't use def for condensec matrix

--- lib/scholar/cluster/hierarchical/condensed_matrix.ex
+++ lib/scholar/cluster/hierarchical/condensed_matrix.ex
@@ -2,18 +2,23 @@ defmodule Scholar.Cluster.Hierarchical.CondensedMatrix do
   import Nx.Defn
 
   # The elements of `d` below the diagonal flattened into a 1D tensor.
-  def condense_pairwise(%Nx.Tensor{} = d) do
-    {n, n} = Nx.shape(d)
+  defn condense_pairwise(%Nx.Tensor{} = d) do
+    {n, _} = Nx.shape(d)
     Nx.gather(d, pairwise_indices(n))
   end
 
-  def pairwise_indices(n), do: Nx.tensor(for(i <- 1..(n - 1), j <- 0..(i - 1), do: [i, j]))
+  # `tri(n) * 2` tensor of row/col indices for the below-diagonal.
+  deftransform pairwise_indices(n) do
+    {div(n * (n - 1), 2)}
+    |> Nx.iota()
+    |> i_to_rc()
+  end
 
   defn rc_to_i(%Nx.Tensor{} = rc), do: tri(rc[[.., 0]] - 1) + rc[[.., 1]]
 
   defn i_to_rc(%Nx.Tensor{} = i) do
-    t = tri_inv(i)
-    Nx.stack([i - tri(t), t + 1])
+    n = tri_inv(i)
+    Nx.stack([n + 1, i - tri(n)], axis: 1)
   end
 
   # nth triangle number, i.e. (n choose 2)
