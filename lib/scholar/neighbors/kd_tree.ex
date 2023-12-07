675048b0dcdc4d96e96af16fe713c456694c52cb
Mateusz <msluszniak@student.agh.edu.pl>
2023-12-08 00:50:33 +0100

    Format

--- lib/scholar/neighbors/kd_tree.ex
+++ lib/scholar/neighbors/kd_tree.ex
@@ -453,11 +453,13 @@ defmodule Scholar.Neighbors.KDTree do
   defnp query_points(point, tree, opts) do
     k = opts[:k]
     node = Nx.as_type(root(), :s64)
-    point = if Nx.flat_size(point) == Nx.size(point) do
-      Nx.vectorize(point, :x)
-    else
-      point
-    end
+
+    point =
+      if Nx.flat_size(point) == Nx.size(point) do
+        Nx.vectorize(point, :x)
+      else
+        point
+      end
 
     {size, dims} = Nx.shape(tree.data)
     nearest_neighbors = Nx.broadcast(Nx.s64(0), {k})
