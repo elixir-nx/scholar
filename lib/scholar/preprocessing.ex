f367e6dbe61bf33658d29cc2bb63a73838322176
Mateusz <msluszniak@student.agh.edu.pl>
2023-12-29 13:20:46 +0100

    Change preprocessing.ex

--- lib/scholar/preprocessing.ex
+++ lib/scholar/preprocessing.ex
@@ -4,30 +4,6 @@ defmodule Scholar.Preprocessing do
   """
 
   import Nx.Defn
-  import Scholar.Shared
-
-  general_schema = [
-    axes: [
-      type: {:custom, Scholar.Options, :axes, []},
-      doc: """
-      Axes to calculate the distance over. By default the distance
-      is calculated between the whole tensors.
-      """
-    ]
-  ]
-
-  normalize_schema =
-    general_schema ++
-      [
-        norm: [
-          type: {:in, [:euclidean, :chebyshev, :manhattan]},
-          default: :euclidean,
-          doc: """
-          The norm to use to normalize each non zero sample.
-          Possible options are `:euclidean`, `:manhattan`, and `:chebyshev`
-          """
-        ]
-      ]
 
   binarize_schema = [
     type: [
@@ -46,7 +22,6 @@ defmodule Scholar.Preprocessing do
     ]
   ]
 
-  @normalize_schema NimbleOptions.new!(normalize_schema)
   @binarize_schema NimbleOptions.new!(binarize_schema)
 
   @doc """
@@ -210,9 +185,8 @@ defmodule Scholar.Preprocessing do
   The zero-tensors cannot be normalized and they stay the same
   after normalization.
 
-  ## Options
-
-  #{NimbleOptions.docs(@normalize_schema)}
+  It is a shortcut for `Scholar.Preprocessing.Normalizer.fit_transform/2`.
+  See `Scholar.Preprocessing.Normalizer` for more information.
 
   ## Examples
 
@@ -237,43 +211,6 @@ defmodule Scholar.Preprocessing do
       >
   """
   deftransform normalize(tensor, opts \\ []) do
-    normalize_n(tensor, NimbleOptions.validate!(opts, @normalize_schema))
-  end
-
-  defnp normalize_n(tensor, opts) do
-    shape = Nx.shape(tensor)
-    type = to_float_type(tensor)
-    zeros = Nx.broadcast(Nx.tensor(0.0, type: type), shape)
-
-    norm =
-      case opts[:norm] do
-        :euclidean ->
-          Scholar.Metrics.Distance.euclidean(tensor, zeros, axes: opts[:axes])
-
-        :manhattan ->
-          Scholar.Metrics.Distance.manhattan(tensor, zeros, axes: opts[:axes])
-
-        :chebyshev ->
-          Scholar.Metrics.Distance.chebyshev(tensor, zeros, axes: opts[:axes])
-
-        other ->
-          raise ArgumentError,
-                "expected :norm to be one of: :euclidean, :manhattan, and :chebyshev, got: #{inspect(other)}"
-      end
-
-    shape_to_broadcast = unsqueezed_reduced_shape(shape, opts[:axes])
-
-    norm =
-      Nx.select(norm == 0.0, Nx.tensor(1.0, type: type), norm) |> Nx.reshape(shape_to_broadcast)
-
-    tensor / norm
-  end
-
-  deftransformp unsqueezed_reduced_shape(shape, axes) do
-    if axes != nil do
-      Enum.reduce(axes, shape, &put_elem(&2, &1, 1))
-    else
-      Tuple.duplicate(1, Nx.rank(shape))
-    end
+    Scholar.Preprocessing.Normalizer.fit_transform(tensor, opts)
   end
 end
