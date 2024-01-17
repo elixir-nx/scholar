9d778a151a6f7c61795966c4aa719474eef1c9d1
Krsto Proroković <krstopro@macbook-pro.t-com.me>
2024-01-17 19:16:24 +0100

    Update.

--- lib/scholar/metrics/neighbors.ex
+++ lib/scholar/metrics/neighbors.ex
@@ -1,34 +1,37 @@
 defmodule Scholar.Metrics.Neighbors do
   import Nx.Defn
 
-  deftransform recall(graph_true, graph_pred) do
-    if Nx.rank(graph_true) != 2 do
+  deftransform recall(neighbors_true, neighbors_pred) do
+    if Nx.rank(neighbors_true) != 2 do
       raise ArgumentError,
             """
-            expected true graph to have shape {num_samples, num_neighbors}, \
-            got tensor with shape: #{inspect(Nx.shape(graph_true))}\
+            expected true neighbors to have shape {num_samples, num_neighbors}, \
+            got tensor with shape: #{inspect(Nx.shape(neighbors_true))}\
             """
     end
-    if Nx.rank(graph_pred) != 2 do
+
+    if Nx.rank(neighbors_pred) != 2 do
       raise ArgumentError,
             """
-            expected predicted graph to have shape {num_samples, num_neighbors}, \
-            got tensor with shape: #{inspect(Nx.shape(graph_pred))}\
+            expected predicted neighbors to have shape {num_samples, num_neighbors}, \
+            got tensor with shape: #{inspect(Nx.shape(neighbors_pred))}\
             """
     end
-    if Nx.shape(graph_true) != Nx.shape(graph_pred) do
+
+    if Nx.shape(neighbors_true) != Nx.shape(neighbors_pred) do
       raise ArgumentError,
             """
-            expected true and predicted graph to have the same shape, \
-            got #{inspect(Nx.shape(graph_true))} and #{inspect(Nx.shape(graph_pred))}\
+            expected true and predicted neighbors to have the same shape, \
+            got #{inspect(Nx.shape(neighbors_true))} and #{inspect(Nx.shape(neighbors_pred))}\
             """
     end
-    recall_n(graph_true, graph_pred)
+
+    recall_n(neighbors_true, neighbors_pred)
   end
 
-  defn recall_n(graph_true, graph_pred) do
-    {n, k} = Nx.shape(graph_true)
-    concatenated = Nx.concatenate([graph_true, graph_pred], axis: 1) |> Nx.sort(axis: 1)
+  defn recall_n(neighbors_true, neighbors_pred) do
+    {n, k} = Nx.shape(neighbors_true)
+    concatenated = Nx.concatenate([neighbors_true, neighbors_pred], axis: 1) |> Nx.sort(axis: 1)
     duplicate_mask = concatenated[[.., 0..(2 * k - 2)]] == concatenated[[.., 1..(2 * k - 1)]]
     duplicate_mask |> Nx.sum() |> Nx.divide(n * k)
   end
