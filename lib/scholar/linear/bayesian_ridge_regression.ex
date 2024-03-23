895dd9751000dae83e5caf07becfa816992f40f2
Joaquin Iglesias Turina <iglesiastj@gmail.com>
2024-03-23 12:08:59 +0100

    bayesian ridge algorithm works for simplest case

--- lib/scholar/linear/bayesian_ridge_regression.ex
+++ lib/scholar/linear/bayesian_ridge_regression.ex
@@ -3,8 +3,8 @@ defmodule Scholar.Linear.BayesianRidgeRegression do
   import Nx.Defn
   import Scholar.Shared
 
-  @derive {Nx.Container, containers: [:coefficients, :intercept]}
-  defstruct [:coefficients, :intercept]
+  @derive {Nx.Container, containers: [:coefficients]}
+  defstruct [:coefficients]
   opts = [
     sample_weights: [
       type:
@@ -57,7 +57,92 @@ defmodule Scholar.Linear.BayesianRidgeRegression do
       If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
       In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
       """
-    ]
+    ],
+    alpha_1: [
+      type:
+        {:or,
+         [
+           {:custom, Scholar.Options, :non_negative_number, []},
+           {:list, {:custom, Scholar.Options, :non_negative_number, []}},
+           {:custom, Scholar.Options, :weights, []}
+         ]},
+      default: 1.0e-6,
+      doc: ~S"""
+      Constant that multiplies the $L_2$ term, controlling regularization strength.
+      `:alpha` must be a non-negative float i.e. in [0, inf).
+
+      If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
+      In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
+      """
+    ],
+    alpha_2: [
+      type:
+        {:or,
+         [
+           {:custom, Scholar.Options, :non_negative_number, []},
+           {:list, {:custom, Scholar.Options, :non_negative_number, []}},
+           {:custom, Scholar.Options, :weights, []}
+         ]},
+      default: 1.0e-6,
+      doc: ~S"""
+      Constant that multiplies the $L_2$ term, controlling regularization strength.
+      `:alpha` must be a non-negative float i.e. in [0, inf).
+
+      If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
+      In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
+      """
+    ],        
+    lambda: [
+      type:
+        {:or,
+         [
+           {:custom, Scholar.Options, :non_negative_number, []},
+           {:list, {:custom, Scholar.Options, :non_negative_number, []}},
+           {:custom, Scholar.Options, :weights, []}
+         ]},
+      default: 1.0,
+      doc: ~S"""
+      Constant that multiplies the $L_2$ term, controlling regularization strength.
+      `:alpha` must be a non-negative float i.e. in [0, inf).
+
+      If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
+      In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
+      """
+    ],
+    lambda_1: [
+      type:
+        {:or,
+         [
+           {:custom, Scholar.Options, :non_negative_number, []},
+           {:list, {:custom, Scholar.Options, :non_negative_number, []}},
+           {:custom, Scholar.Options, :weights, []}
+         ]},
+      default: 1.0e-6,
+      doc: ~S"""
+      Constant that multiplies the $L_2$ term, controlling regularization strength.
+      `:alpha` must be a non-negative float i.e. in [0, inf).
+
+      If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
+      In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
+      """
+    ],
+    lambda_2: [
+      type:
+        {:or,
+         [
+           {:custom, Scholar.Options, :non_negative_number, []},
+           {:list, {:custom, Scholar.Options, :non_negative_number, []}},
+           {:custom, Scholar.Options, :weights, []}
+         ]},
+      default: 1.0e-6,
+      doc: ~S"""
+      Constant that multiplies the $L_2$ term, controlling regularization strength.
+      `:alpha` must be a non-negative float i.e. in [0, inf).
+
+      If `:alpha` is set to 0.0 the objective is the ordinary least squares regression.
+      In this case, for numerical reasons, you should use `Scholar.Linear.LinearRegression` instead.
+      """
+    ],        
   ]
   @opts_schema NimbleOptions.new!(opts)
   deftransform fit(x, y, opts \\ []) do
@@ -70,29 +155,91 @@ defmodule Scholar.Linear.BayesianRidgeRegression do
         do: Nx.as_type(sample_weights, x_type),
         else: Nx.tensor(sample_weights, type: x_type)
 
+    ## TODO: refactor this
     {alpha, opts} = Keyword.pop!(opts, :alpha)
     alpha = Nx.tensor(alpha, type: x_type) |> Nx.flatten()
+    {alpha_1, opts} = Keyword.pop!(opts, :alpha_1)
+    alpha_1 = Nx.tensor(alpha_1, type: x_type) |> Nx.flatten()
+    {alpha_2, opts} = Keyword.pop!(opts, :alpha_2)
+    alpha_2 = Nx.tensor(alpha_2, type: x_type) |> Nx.flatten()    
+    {lambda, opts} = Keyword.pop!(opts, :lambda)
+    lambda = Nx.tensor(lambda, type: x_type) |> Nx.flatten()
+    {lambda_1, opts} = Keyword.pop!(opts, :lambda_1)
+    lambda_1 = Nx.tensor(lambda_1, type: x_type) |> Nx.flatten()    
+    {lambda_2, opts} = Keyword.pop!(opts, :lambda_2)
+    lambda_2 = Nx.tensor(lambda_2, type: x_type) |> Nx.flatten()    
+    
     num_targets = if Nx.rank(y) == 1, do: 1, else: Nx.axis_size(y, 1)
-
     if Nx.size(alpha) not in [0, 1, num_targets] do
       raise ArgumentError,
       "expected number of targets be the same as number of penalties, got: #{inspect(num_targets)} != #{inspect(Nx.size(alpha))}"
     end
-
-    fit_n(x, y, sample_weights, alpha, opts)
+    {coefficients, _rmse,
+     convergence, iterations} = fit_n(x, y, sample_weights,
+                                      alpha, lambda,
+                                      alpha_1, alpha_2, lambda_1, lambda_2)
+    %__MODULE__{coefficients: coefficients}
   end
 
-  defnp fit_n(a, b, sample_weights, alpha, opts) do
-    {u, s, vh} = Nx.LinAlg.svd(a, full_matrices?: false)
-    eigen_vals = Nx.pow(s, 2)
-    %__MODULE__{coefficients: Nx.tensor([1, 2, 3]), intercept: Nx.tensor([1])}
+  deftransformp fit_n(x, y, sample_weights,
+                      alpha, lambda,
+                      alpha_1, alpha_2, lambda_1, lambda_2) do
+    xt_y = Nx.dot(Nx.transpose(x), y)
+    {u, s, vh} = Nx.LinAlg.svd(x, full_matrices?: false)
+    eigenvals = Nx.pow(s, 2)
+    {n_samples, n_features} = Nx.shape(x)
+    initial_coefficients = Nx.tensor(for _n <- 0..(n_features - 1), do: 0)
+    update_coef_iterations(
+      x, y, n_samples, n_features,
+      xt_y, u, vh, eigenvals,
+      alpha, lambda,
+      alpha_1, alpha_2, lambda_1, lambda_2,      
+      300, initial_coefficients)
+  end
+
+  deftransformp update_coef_iterations(
+          x, y, n_samples, n_features,
+          xt_y, u, vh, eigenvals,
+          alpha, lambda,
+          alpha_1, alpha_2, lambda_1, lambda_2,
+          iterations, coef_old) do
+    {coefficients, rmse} = update_coef(
+                              x, y, n_samples, n_features,
+                              xt_y, u, vh, eigenvals,
+                              alpha, lambda)
+    {gamma, lambda, alpha} = update_hyperparameters(
+                               n_samples, coefficients, rmse,
+                               eigenvals, alpha, lambda,
+                               alpha_1, alpha_2, lambda_1, lambda_2)
+    convergence = check_convergence(
+                    coef_old, coefficients, Nx.tensor(1.0e-3))
+    |> Nx.to_number()
+    
+    if ((convergence == 1) or (iterations <= 0)) do
+      {coefficients, rmse, convergence == 1, iterations}
+    else
+      update_coef_iterations(
+        x, y, n_samples, n_features,
+        xt_y, u, vh, eigenvals,
+        alpha, lambda, alpha_1, alpha_2, lambda_1, lambda_2,
+        iterations - 1, coefficients)
+    end
+  end
+
+  defnp update_hyperparameters(n_samples, coefficients, rmse,
+                               eigenvals, alpha, lambda,
+                               alpha_1, alpha_2, lambda_1, lambda_2) do
+    gamma = Nx.sum((alpha * eigenvals) / (lambda + alpha * eigenvals))
+    lambda = (gamma + 2 * lambda_1) / (Nx.sum(coefficients ** 2) + 2 * lambda_2)
+    alpha = (n_samples - gamma + 2 * alpha_1) / (rmse + 2 * alpha_2)
+    {gamma, lambda, alpha}
   end
 
   defnp update_coef(
           x, y, n_samples, n_features, xt_y,
-          u, vh, eigen_vals,
+          u, vh, eigenvals,
           alpha, lambda) do
-    regularization = vh / (eigen_vals + lambda / alpha)
+    regularization = vh / (eigenvals + lambda / alpha)
     reg_transpose = Nx.dot(regularization, xt_y)
     coef = Nx.dot(Nx.transpose(vh), reg_transpose)
 
@@ -102,4 +249,13 @@ defmodule Scholar.Linear.BayesianRidgeRegression do
 
     {coef, rmse}
   end
+
+  defnp check_convergence(coef_old, coef_new, tol) do
+    Nx.less(Nx.sum(Nx.abs(coef_old - coef_new)), tol)
+  end
+  
+  defn predict(%__MODULE__{coefficients: coeff} = _model, x) do
+    Nx.dot(x, coeff)
+  end
+  
 end
--- test/scholar/linear/bayesian_ridge_regression_test.exs
+++ test/scholar/linear/bayesian_ridge_regression_test.exs
@@ -5,26 +5,11 @@ defmodule Scholar.Linear.BayesianRidgeRegressionTest do
 
   test "toy bayesian ridge" do
     x = Nx.tensor([[1], [2], [6], [8], [10]])
-    {u, s, vh} = Nx.LinAlg.svd(x, full_matrices?: false)
-    eigen_vals = Nx.pow(s, 2)
     y = Nx.tensor([1, 2, 6, 8, 10])
-    alpha = 1
-    lambda = 1
-    xt_y = Nx.dot(Nx.transpose(x), y)
-    IO.inspect(xt_y)
-    regularization = # vh / (eigen_vals + lambda / alpha)
-      Nx.divide(vh, Nx.divide(Nx.add(eigen_vals, lambda), alpha))
-    IO.inspect(regularization)
-    reg_transpose = Nx.dot(regularization, xt_y)
-    coef = Nx.dot(Nx.transpose(vh), reg_transpose)
-    IO.inspect(coef)
-    
-    
     clf = BayesianRidgeRegression.fit(x, y)
     test = Nx.tensor([[1], [3], [4]])
-    expected_predict = Nx.tensor([1, 3, 4])
-
-    assert false
+    expected = Nx.tensor([1, 3, 4])
+    predicted = BayesianRidgeRegression.predict(clf, test)
+    assert_all_close(expected, predicted)
   end
-  
 end
