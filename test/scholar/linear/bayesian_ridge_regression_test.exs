1a3070ce9be2860b69c86e598c2e0784fe7e714f
iglesiastj <iglesiastj@gmail.com>
2024-03-26 18:16:06 +0100

    add test case

--- test/scholar/linear/bayesian_ridge_regression_test.exs
+++ test/scholar/linear/bayesian_ridge_regression_test.exs
@@ -12,4 +12,16 @@ defmodule Scholar.Linear.BayesianRidgeRegressionTest do
     predicted = BayesianRidgeRegression.predict(clf, test)
     assert_all_close(expected, predicted, atol: 1.0e-1)
   end
+  
+  test "toy bayesian ride expanded" do
+    x = Nx.tensor([[1, 1], [2, 2], [6, 6], [8, 8], [10, 10]])
+    y = Nx.tensor([1, 2, 6, 8, 10])
+    clf = BayesianRidgeRegression.fit(x, y)
+    test = Nx.tensor([[1, 1], [3, 3], [4, 4]])
+    expected = Nx.tensor([1, 3, 4])
+    predicted = BayesianRidgeRegression.predict(clf, test)
+    IO.inspect(clf)
+    IO.inspect(predicted)
+    assert false            
+  end  
 end
