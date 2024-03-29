889de37c0249e8257ce8866858b8ba02c8279a1f
Joaquin Iglesias Turina <iglesiastj@gmail.com>
2024-03-29 13:05:50 +0100

    working on test to compute score

--- test/scholar/linear/bayesian_ridge_regression_test.exs
+++ test/scholar/linear/bayesian_ridge_regression_test.exs
@@ -1,4 +1,5 @@
 defmodule Scholar.Linear.BayesianRidgeRegressionTest do
+  import Nx.Defn  
   use Scholar.Case, async: true
   alias Scholar.Linear.BayesianRidgeRegression
   alias Scholar.Linear.RidgeRegression
@@ -33,8 +34,49 @@ defmodule Scholar.Linear.BayesianRidgeRegressionTest do
     assert_all_close(brr.intercept, rr.intercept, atol: 1.0e-2)
   end
 
+  @tag :wip
   test "compute scores" do
+    {x, y, noise} = data()
+
+    eps = Nx.Constants.smallest_positive_normal({:f, 64})
+    alpha = Nx.divide(1, Nx.add(Nx.variance(x), eps))
+    lambda = Nx.tensor(1.0)
+    alpha_1 = 0.1
+    alpha_2 = 0.1
+    lambda_1 = 0.1
+    lambda_2 = 0.1
+    # compute score
+    score = compute_score(x, y, alpha, lambda, alpha_1, alpha_2, lambda_1, lambda_2)
+    IO.inspect(score)
+    brr = BayesianRidgeRegression.fit(x, y,
+      alpha_1: alpha_1, alpha_2: alpha_2, lambda_1: lambda_1, lambda_2: lambda_2,
+      fit_intercept?: false, iterations: 1)
     assert false
+  end
+
+  defnp compute_score(x, y, alpha, lambda, alpha_1, alpha_2, lambda_1, lambda_2) do
+    {n_samples, _} = Nx.shape(x)
+    score_lambda = lambda_1 + Nx.log(lambda) - lambda_2 * lambda
+    score_alpha =  alpha_1 + Nx.log(alpha_2) - alpha_2 * alpha
+    m =  (1.0 / alpha) * Nx.eye(n_samples) + (1.0 / lambda) * Nx.dot(x, Nx.transpose(x))
+    m_inv_dot_y = Nx.LinAlg.solve(m, y)
+    logdet = m |> Nx.LinAlg.determinant |> Nx.log()
+    y_score = -0.5 * (logdet + Nx.dot(y, m_inv_dot_y) +
+                        n_samples * Nx.log(2 * Nx.Constants.pi()))
+    
+    score_alpha + score_lambda + y_score
+  end
+
+  # copied from the linear regression notebook
+  # https://hexdocs.pm/scholar/linear_regression.html
+  defnp data do
+    key = Nx.Random.key(42)
+    size = 30
+    {x, new_key} = Nx.Random.normal(key, 0, 2, shape: {size, 3}, type: :f64)
+    {noise, _} = Nx.Random.normal(new_key, -0.5, 0.5, shape: {size}, type: :f64)
+    w = Nx.tensor([1, 2, 3])
+    y = Nx.dot(x, w) + 4
+    {x, y, noise}
   end
 
   test "constant inputs: prediction" do
