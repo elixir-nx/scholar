936d3f792a1ae2895d6a8f6ad6392a4c407c2e08
norm4nn <szymon.jan.mamon@gmail.com>
2024-10-02 22:30:48 +0200

    run mix format

--- lib/scholar/decomposition/utils.ex
+++ lib/scholar/decomposition/utils.ex
@@ -2,7 +2,7 @@ defmodule Scholar.Decomposition.Utils do
   @moduledoc false
   import Nx.Defn
   require Nx
-  
+
   defn flip_svd(u, v) do
     max_abs_cols_idx = u |> Nx.abs() |> Nx.argmax(axis: 0, keep_axis: true)
     signs = u |> Nx.take_along_axis(max_abs_cols_idx, axis: 0) |> Nx.sign() |> Nx.squeeze()
