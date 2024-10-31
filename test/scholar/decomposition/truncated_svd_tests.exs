15662faedcad0d6e1244d9e61f77d686c4d8f99c
norm4nn <szymon.jan.mamon@gmail.com>
2024-10-31 07:33:01 +0100

    xd again tsvd bugfix

--- test/scholar/decomposition/truncated_svd_tests.exs
+++ test/scholar/decomposition/truncated_svd_tests.exs
@@ -3,17 +3,27 @@ defmodule Scholar.Decomposition.TruncatedSVDTest do
   alias Scholar.Decomposition.TruncatedSVD
   doctest TruncatedSVD
 
-  defp x do
-    Nx.tensor()
+  defp key do
+    Nx.Random.key(1)
   end
+
+  defp x do
+    key = key()
+    {x, _new_key} = Nx.Random.multivariate_normal(key, Nx.tensor([0.0, 0.0, 0.0, 0.0]), Nx.tensor([[3.0, 2.0, 1.0, 9.0], [1.0, 2.0, 3.0, 8.2], [1.3, 1.0, 2.2, 2.4], [1.8, 1.0, 2.0, 2.9]]), shape: {50}, type: :f32)
+    x
+  end
+  # key = Nx.Random.key(1)
+  # {x, _new_key} = Nx.Random.multivariate_normal(key, Nx.tensor([0.0, 0.0, 0.0, 0.0]), Nx.tensor([[3.0, 2.0, 1.0, 9.0], [1.0, 2.0, 3.0, 8.2], [1.3, 1.0, 2.2, 2.4], [1.8, 1.0, 2.0, 2.9]]), shape: {50}, type: :f32)
+  # tsvd = Scholar.Decomposition.TruncatedSVD.fit_transform(x, num_components: 2, key: key)
 
 
   test "fit test - all default options" do
-    
+    key = key()
+    x = x()
   end
 
   test "fit_transform test - all default options" do
-    
+    #tsvd = Scholar.Decomposition.TruncatedSVD.fit_transform(x, num_components: 2, key: key)
   end
 
   test "fit_transform test - :num_components" do
