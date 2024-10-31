ac119e0d0a45b20c8965705bcd1bec56152aadea
norm4nn <szymon.jan.mamon@gmail.com>
2024-10-31 06:53:15 +0100

    added test module

---
+++ test/scholar/decomposition/truncated_svd_tests.exs
@@ -0,0 +1,30 @@
+defmodule Scholar.Decomposition.TruncatedSVDTest do
+  use Scholar.Case, async: true
+  alias Scholar.Decomposition.TruncatedSVD
+  doctest TruncatedSVD
+
+  defp x do
+    Nx.tensor()
+  end
+
+
+  test "fit test - all default options" do
+    
+  end
+
+  test "fit_transform test - all default options" do
+    
+  end
+
+  test "fit_transform test - :num_components" do
+    
+  end
+
+  test "fit_transform test - :num_oversamples" do
+    
+  end
+
+  test "fit_transform test - :num_iters" do
+    
+  end
+end
